18ca12b2d2aa52bd852ef446f4e10a6acdcad270
Maria Reyna <maria.reyna2244@gmail.com>
2023-02-15 09:17:32 -0500

    Updated the gameplay to show the correct answer now

--- Climb/Game.swift
+++ Climb/Game.swift
@@ -11,7 +11,7 @@ import Foundation
 class Math: ObservableObject{
     @Published var isAnswerCorrect = false
      private(set) var correctAnswer = 0
-   @Published var choicearry : [Int] = [0,1,2,3,4,5,6,7,8,9,10,11]
+   @Published var choicearry : [Int] = [0,1,2,3,4,5,6,7,8,9]
      private(set) var firstNum = 0
      private(set) var secondNum = 0
      private(set) var difficulty = 150
@@ -23,7 +23,6 @@ class Math: ObservableObject{
               self.score += 1
               self.timeRemaining += 5
               self.isAnswerCorrect = true
-
               return true
           }else{
               if self.score < 1{
@@ -37,18 +36,16 @@ class Math: ObservableObject{
       }
 
       func generateAnswers(){
-          if self.isAnswerCorrect == false {
               self.firstNum = Int.random(in: 0...(difficulty/2))
               self.secondNum = Int.random(in: 0...(difficulty/2))
               var answerList = [Int]()
               correctAnswer =  self.firstNum + self.secondNum
                             
-              for _ in 0...11{
+              for _ in 0...9{
                   answerList.append(Int.random(in: 0...difficulty))
               }
               answerList.append(correctAnswer)
               choicearry = answerList.shuffled()
-          }
       }
   }
 
