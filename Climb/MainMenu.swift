8c2abca4d1bfaaa210d66247dd19de7abe45d8de
Maria Reyna <maria.reyna2244@gmail.com>
2023-02-17 10:04:13 -0500

    added navigation links between screens

--- Climb/MainMenu.swift
+++ Climb/MainMenu.swift
@@ -11,68 +11,67 @@ import SwiftUI
 struct MainMenuView: View {
     @State private var username = "Hadi" // replace with user's actual name
     var body: some View {
-        
-    
-        ZStack {
-            VStack {
-                HStack {
+        NavigationStack{
+            ZStack {
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            // handle setting button action
+                        }) {
+                            Image(systemName: "gear")
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(height:-500)
+                            Spacer()
+                        }
+                    }
+                    Spacer()
+                    Image(systemName: "person.crop.circle.fill")
+                    
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 80)
+                    Text("Welcome, \(username)!")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .background(Rectangle())
                     Spacer()
                     Button(action: {
-                        // handle setting button action
+                        // handle "Continue" button action
                     }) {
-                        Image(systemName: "gear")
-                            .foregroundColor(.white)
+                        Text("Continue")
                             .padding()
-                            .frame(height:-500)
-                            Spacer()
+                            .background(Color.white)
+                            .foregroundColor(.blue)
+                            .cornerRadius(10)
+                        
+                        
                     }
-                }
-                Spacer()
-                Image(systemName: "person.crop.circle.fill")
-                
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 80, height: 80)
-                Text("Welcome, \(username)!")
-                    .font(.title)
-                    .foregroundColor(.white)
-                Spacer()
-                Button(action: {
-                    // handle "Continue" button action
-                }) {
-                    Text("Continue")
-                        .padding()
-                        .background(Color.white)
-                        .foregroundColor(.blue)
-                        .cornerRadius(10)
-                       
+                    NavigationLink(destination: ContentView(), label: {
+                            Text("New Game")
 
-                }
-                Button(action: {
-                    // handle "New Game" button action
-                }) {
-                    Text("New Game")
+                                .padding()
+                                .background(Color.white)
+                                .foregroundColor(.blue)
+                                .cornerRadius(10)
+                        }
+                        )
+                    NavigationLink(destination: LeaderBoardView(), label: {
+                        Text("Leaderboard")
+                            .padding()
+                            .background(Color.white)
+                            .foregroundColor(.blue)
+                            .cornerRadius(10)
+                    }
+                                   )
+                    Spacer()
+                    Spacer()
                     
-                        .padding()
-                        .background(Color.white)
-                        .foregroundColor(.blue)
-                        .cornerRadius(10)
+                    
                 }
-                Button(action: {
-                    // handle "Leaderboard" button action
-                }) {
-                    Text("Leaderboard")
-                        .padding()
-                        .background(Color.white)
-                        .foregroundColor(.blue)
-                        .cornerRadius(10)
-                }
-                Spacer()
-                Spacer()
-                
-                
+                .background(Image("mainMenuBackground"))
             }
-            .background(Image("mainMenuBackground"))
         }
     }
 }
