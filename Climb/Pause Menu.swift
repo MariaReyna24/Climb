d80aef06e02c5ae42ac10993a0c1b2d8cb87b7ef
Maria Reyna <maria.reyna2244@gmail.com>
2023-04-13 11:18:48 -0400

    Fixed up the pause screen so it matches the rest of the screens

--- Climb/Pause Menu.swift
+++ Climb/Pause Menu.swift
@@ -13,32 +13,50 @@ struct Pause_menu: View {
     @Environment(\.dismiss) var dismiss
     @ObservedObject var game : Math
     var body: some View {
-        ZStack{
+        NavigationView{
             VStack{
                 Text("Current score: \(game.score)")
                     .font(.largeTitle)
-                Text("Time Remaining: \(game.timeRemaining)")
-                    .font(.largeTitle)
+                    .foregroundColor(.red)
+                    .fontWeight(.bold)
                 Button("Resume") {
                     dismiss()
                     game.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
                 }
                 .font(.title)
                 .padding()
-                .background(.black)
-                .foregroundColor(.red)
-            }    .background((Image("background")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-                .frame(width: 393, height: 918)
-            ))
+                .background(Color("myColor"))
+                .foregroundColor(Color("textColor"))
+            }.navigationTitle("Level \(game.levelnum)")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarLeading){
+                        Button("Paused"){
+                            
+                        }.font(.title2)
+                            .foregroundColor(.red)
+                    }
+                    //timer in the right hand corner
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        Text("\(game.timeRemaining)s")
+                            .font(.system(size: 30))
+                            .foregroundColor(.red)
+                            .fontWeight(.bold)
+                    }
+                }
+                .background((Image("background")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .frame(width: 393, height: 918)
+                ))
         }
+    }
+    
+}
+struct Pause_menu_Previews: PreviewProvider {
+    static var previews: some View {
+        Pause_menu(game: Math())
     }
 }
-    struct Pause_menu_Previews: PreviewProvider {
-        static var previews: some View {
-            Pause_menu(game: Math())
-        }
-    }
 
