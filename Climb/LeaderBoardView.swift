4a63decadbeac54b2b35dff8f764f0d099d0227b
Maria Reyna <maria.reyna2244@gmail.com>
2023-05-11 09:11:53 -0400

    adding things from old leaderboard branch

--- Climb/LeaderBoardView.swift
+++ Climb/LeaderBoardView.swift
@@ -6,47 +6,79 @@
 //
 
 import SwiftUI
+import GameKit
+
+struct Player: Hashable, Comparable {
+    static func < (lhs: Player, rhs: Player) -> Bool {
+        return rhs.score > lhs.score
+    }
+    
+    let id = UUID()
+    let name: String
+    let score: Int
+}
 
 struct LeaderBoardView: View {
-    var friends = ["Maria","Daniel","Delon","Hadi","Michael"]
-    var highScores = [
-        HighScore(place: 1, name: "Maria", points: "100"),
-        HighScore(place: 2, name: "Daniel", points: "75"),
-        HighScore(place: 3, name: "Delon", points: "50"),
-        HighScore(place: 4, name: "Hadi", points: "25"),
-        HighScore(place: 5, name: "Michael", points: "15")
-    ]
-    
-    let highscore = ["100 Points","75 Points","50 Points","25 Points","15 Points" ]
-    
+    @StateObject var game = Math()
+    @State var playersList: [Player] = []
+    var leaderboardIdentifier = "climb.Leaderboard"
     var body: some View {
         ZStack{
             VStack {
-                    List{
-                        Text("Leaderboard")
-                            .font(.largeTitle)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 350)
-                        ForEach(highScores, id: \.place) { highScore in
-                            HStack {
-                                Text("\(highScore.place). \(highScore.name)")
-                                Spacer()
-                                Text("\(highScore.points) Points")
-                            }
-                        }
-                    }.scrollContentBackground(.hidden)
+                Text("Leaderboard")
+                ScrollView{
+                    ForEach(playersList, id: \.id) { player in
+                        Text("\(String(player.name.prefix(8))) Score: \(player.score)")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundColor(.black)
+                            .font(.system(size: 15))
+                            .textCase(.uppercase)
+                    }
+                }
             }
+            .onAppear(){
+                if !GKLocalPlayer.local.isAuthenticated {
+                    game.authenticateUser()
+                } else if playersList.count == 0 {
+                    Task{
+                        await loadLeaderboard()
+                    }
+                }
+            }
+            
         }.background(Image("background")
             .resizable()
             .scaledToFill()
             .ignoresSafeArea()
             .frame(width: 393, height: 918))
-            
-            
+        
+        
         
     }
-    
+    func loadLeaderboard() {
+        playersList.removeAll()
+        Task{
+            var playersListTemp : [Player] = []
+            let leaderboards = try await GKLeaderboard.loadLeaderboards(IDs: [leaderboardIdentifier])
+            if let leaderboard = leaderboards.filter ({ $0.baseLeaderboardID == self.leaderboardIdentifier }).first {
+                let allPlayers = try await leaderboard.loadEntries(for: .global, timeScope: .allTime, range: NSRange(1...10))
+                if allPlayers.1.count > 0 {
+                    for leaderboardEntry in allPlayers.1 {
+                        playersListTemp.append(Player(name: leaderboardEntry.player.displayName, score:leaderboardEntry.score))
+                                    print(playersListTemp)
+
+                    }
+                }
+            }
+            playersList = playersListTemp
+            playersList = playersList.sorted {
+                $0.score < $1.score
+            }
+            playersList.reverse()
+        }
+    }
 }
+
 
 struct LeaderBoardView_Previews: PreviewProvider {
     static var previews: some View {
