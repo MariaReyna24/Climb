f523f3b404c6fb636f18cbe81277c71a2e05e041
hc <hc.adp@icloud.com>
2023-06-16 19:16:25 -0400

    updated pause and settings design

--- Climb/Views/SettingsView.swift
+++ Climb/Views/SettingsView.swift
@@ -57,145 +57,130 @@ struct SettingsView: View {
             
             
             GeometryReader { geometry in
-                VStack {
+                VStack(spacing:30) {
                     
                     
-                    Form {
                         haptics
                         sound
                         
                     }.scrollContentBackground(.hidden)
-                        .offset(y: -0.80 * geometry.size.height)
+                        .offset(y: -0.70 * geometry.size.height)
+                        .offset(x: 0.1 * geometry.size.height)
                 }
             }
             
         }
         
     }
-}
+
 
 
 private extension SettingsView {
     var haptics: some View {
-        Toggle("Enable Haptics", isOn: $isHapticsEnabled)
-            .toggleStyle(HapticsToggleStyle(isSoundEnabled: isSoundEnabled))
-            .listRowBackground(Color("whiteOpacity"))
-    }
-}
-
-struct HapticsToggleStyle: ToggleStyle {
-    @AppStorage(UserDefaultKeys.hapticsEnabled) var isHapticsEnabled: Bool = true
-    var isSoundEnabled: Bool
-    
-    func makeBody(configuration: Configuration) -> some View {
         HStack {
-            configuration.label
-                .frame(height: 75)
-                .foregroundColor(Color.white)
-                .font(.custom("RoundsBlack", size: 27))
+            Text("Haptics")
+                .font(.custom("RoundsBlack", size: 35))
+                .foregroundColor(Color("textColor"))
+                .padding()
             
             Spacer()
             
             Button(action: {
-                configuration.isOn.toggle()
+                isHapticsEnabled.toggle()
                 if isSoundEnabled {
                     SoundManager.instance.playSound(sound: .click)
                 }
             }) {
-                ZStack {
-                    Image(systemName: isHapticsEnabled ? "hand.point.up.left.fill" : "hand.point.up.left")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(.white)
-                }
-                .frame(width: 50, height: 50)
-                .background(
-                    Circle()
-                        .fill(configuration.isOn ? Color.green : Color.gray)
-                        .frame(width: 50, height: 50)
-                )
-            }
-            .frame(width: 50, height: 50)
-            .background(
-                RoundedRectangle(cornerRadius: 35)
-                    .fill(Color("whiteOpacity"))
+                Image(systemName: isHapticsEnabled ? "hand.point.up.left.fill" : "hand.point.up.left")
+                    .font(.system(size: 35))
+                    .foregroundColor(.white)
+                    .frame(width: 60, height: 60)
+                    .background(isHapticsEnabled ? Color.green : Color.gray)
+                    .cornerRadius(20)
                     .overlay(
-                        RoundedRectangle(cornerRadius: 35)
-                            .stroke(Color("WhiteDM"), lineWidth: 5)
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color("WhiteDM"), lineWidth: 4)
                     )
-                    .clipShape(RoundedRectangle(cornerRadius: 50))
-            )
-            .shadow(color: Color("pauseColor"), radius: 5, x: 0, y: 0)
-            .padding(0.5)
-            .controlSize(.large)
+                    .shadow(
+                        color: Color.black.opacity(0.5),
+                        radius: 4,
+                        x: 0,
+                        y: 0
+                    )
+                    .shadow(color: Color("WhiteDM"), radius: 6, x: 0, y: 0)
+            }
         }
+        .padding(35)
+        .frame(width:340)
+        .frame(height: 110)
+        .background(Color("pauseColor"))
+        .cornerRadius(25)
+        .overlay(
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(Color("WhiteDM"), lineWidth: 6)
+        )
+        .shadow(
+            color: Color.black.opacity(0.5),
+            radius: 6,
+            x: 0,
+            y: 0
+        )
     }
 }
 
 private extension SettingsView {
     var sound: some View {
-        Toggle("Enable Sound", isOn: $isSoundEnabled)
-            .toggleStyle(SoundToggleStyle(isHapticsEnabled: isHapticsEnabled))
-            .listRowBackground(Color("whiteOpacity"))
-    }
-}
-
-struct SoundToggleStyle: ToggleStyle {
-    @AppStorage(UserDefaultKeys.soundEnabled) var isSoundEnabled: Bool = true
-    var isHapticsEnabled: Bool
-    
-    func makeBody(configuration: Configuration) -> some View {
         HStack {
-            configuration.label
-                .frame(height: 75)
-                .foregroundColor(Color.white)
-                .font(.custom("RoundsBlack", size: 25))
+            Text("Sound")
+                .font(.custom("RoundsBlack", size: 38))
+                .foregroundColor(Color("textColor"))
+                .padding(23)
+                .offset(x:-5)
             
             Spacer()
             
             Button(action: {
-                configuration.isOn.toggle()
-                if isHapticsEnabled {
-                    heavyHaptic()
-                }
+                isSoundEnabled.toggle()
                 if isSoundEnabled {
                     SoundManager.instance.playSound(sound: .click)
                 }
             }) {
-                ZStack {
-                    Image(systemName: isSoundEnabled ? "speaker.wave.3.fill" : "speaker.slash.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(.white)
-                }
-                .frame(width: 50, height: 50)
-                .background(
-                    Circle()
-                        .fill(configuration.isOn ? Color.green : Color.gray)
-                        .frame(width: 50, height: 50)
-                )
-            }
-            .frame(width: 50, height: 50)
-            .background(
-                RoundedRectangle(cornerRadius: 35)
-                    .fill(Color("whiteOpacity"))
+                Image(systemName: isSoundEnabled ? "speaker.wave.3.fill" : "speaker.slash.fill")
+                    .font(.system(size: 32))
+                    .foregroundColor(.white)
+                    .frame(width: 60, height: 60)
+                    .background(isSoundEnabled ? Color.green : Color.gray)
+                    .cornerRadius(20)
                     .overlay(
-                        RoundedRectangle(cornerRadius: 35)
-                            .stroke(Color("WhiteDM"), lineWidth: 5)
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color("WhiteDM"), lineWidth: 4)
                     )
-                    .clipShape(RoundedRectangle(cornerRadius: 40))
-            )
-            .shadow(color: Color("pauseColor"), radius: 5, x: 0, y: 0)
-            .padding(0.5)
-            .controlSize(.large)
+                    .shadow(
+                        color: Color.black.opacity(0.5),
+                        radius: 4,
+                        x: 0,
+                        y: 0
+                    )
+                    .shadow(color: Color("WhiteDM"), radius: 6, x: 0, y: 0)
+            }
         }
+        .padding(35)
+        .frame(width:340)
+        .frame(height: 110)
+        .background(Color("pauseColor"))
+        .cornerRadius(25)
+        .overlay(
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(Color("WhiteDM"), lineWidth: 6)
+        )
+        .shadow(
+            color: Color.black.opacity(0.5),
+            radius: 6,
+            x: 0,
+            y: 0
+        )
     }
 }
-
-
-
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
