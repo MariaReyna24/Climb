75d14a2d450aa9dd133aa472ac04b64c52d847cf
hc <hc.adp@icloud.com>
2023-06-19 10:18:41 -0400

    updated pause menu, leaderboard, and click volume

--- Climb/Sounds/SoundsManager.swift
+++ Climb/Sounds/SoundsManager.swift
@@ -1,21 +1,13 @@
-//
-//  SoundsManager.swift
-//  Climb
-//
-//  Created by Sandra Smothers on 5/19/23.
-//
-
 import SwiftUI
 import AVKit
 
 class SoundManager: ObservableObject {
-    
     static let instance = SoundManager()
     
     var player: AVAudioPlayer?
+    var volume: Float = 1.0 // Default volume
     
     enum SoundOption: String {
-
         case chime
         case fail
         case wrong
@@ -27,11 +19,10 @@ class SoundManager: ObservableObject {
         guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
         do {
             player = try AVAudioPlayer(contentsOf: url)
+            player?.volume = sound == .click ? volume * 0.25 : volume
             player?.play()
         } catch let error {
             print("Error playing sound. \(error.localizedDescription)")
         }
     }
 }
-
-
