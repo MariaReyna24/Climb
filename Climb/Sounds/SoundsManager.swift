57886d2b6a6e150b487e8a895ffde93e770d4810
Maria Reyna <Maria.reyna2244@gmail.com>
2023-06-19 11:15:54 -0400

    fixed sound manager

--- Climb/Sounds/SoundsManager.swift
+++ Climb/Sounds/SoundsManager.swift
@@ -3,8 +3,10 @@ import AVKit
 import AVFoundation
 
 class SoundManager: ObservableObject {
+    var player: AVAudioPlayer?
     static let instance = SoundManager()
     private var players: [SoundOption: AVAudioPlayer] = [:]
+    var volume: Float = 1.0 // Default volume
     
     enum SoundOption: String, CaseIterable {
         case chime
@@ -32,15 +34,27 @@ class SoundManager: ObservableObject {
     }
     
     func playSound(sound: SoundOption) {
-        guard let player = players[sound] else { return }
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
         
         let isSilentMode = isDeviceInSilentMode()
         if isSilentMode {
             return // Exit early if in silent mode
         }
         
-        player.currentTime = 0
-        player.play()
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            adjustVolume(for: sound)
+            player?.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+    private func adjustVolume(for sound: SoundOption) {
+        if sound == .click {
+            player?.volume = volume * 0.25 // Adjust volume for click sound
+        } else {
+            player?.volume = volume // Use default volume for other sounds
+        }
     }
     
     private func isDeviceInSilentMode() -> Bool {
@@ -48,3 +62,5 @@ class SoundManager: ObservableObject {
         return currentCategory == .soloAmbient
     }
 }
+
+
